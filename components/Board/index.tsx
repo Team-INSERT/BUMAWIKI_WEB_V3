618014e4c7fa84906e4bbf1b857d86d2f044dbfd
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 17:11:57 +0900

    feat(board): 보드 컴포넌트 퍼블리싱

---
+++ components/Board/index.tsx
@@ -0,0 +1,28 @@
+import React, { PropsWithChildren } from "react";
+import Image from "next/image";
+import * as styles from "./style.css";
+
+const Board = ({ children }: PropsWithChildren) => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.board}>{children}</main>
+      <div className={styles.subFooter}>
+        <Image
+          className={styles.subFooterLogo}
+          width={80}
+          height={54}
+          src="/assets/logoBlack.png"
+          alt="footer logo"
+        />
+        <span className={styles.subFooterNoticeText}>
+          부마위키는 공식 역사서 및 백과사전이 아니며 검증되지 않았거나,
+          편향적이거나, 잘못된 서술이 있을 수 있습니다.
+          <br />
+          질문이나 특정 사항에 대해 언제든지 문의주실 수 있습니다.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
---
+++ components/Board/style.css.ts
@@ -0,0 +1,35 @@
+import { theme, flex, font } from "@/styles";
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  width: "72vw",
+  ...flex.COLUMN_VERTICAL,
+  borderLeft: `2px solid ${theme.gray}`,
+  borderRight: `2px solid ${theme.gray}`,
+  backgroundColor: theme.white,
+});
+
+export const board = style({
+  width: "100%",
+  height: "fit-content",
+  minHeight: "100svh",
+  overflow: "hidden",
+  padding: "30px",
+  ...flex.COLUMN_FLEX,
+});
+
+export const subFooter = style({
+  width: "96%",
+  borderTop: `2px solid ${theme.gray}`,
+  padding: "10px 0",
+  ...flex.COLUMN_FLEX,
+});
+
+export const subFooterLogo = style({
+  marginLeft: "auto",
+});
+
+export const subFooterNoticeText = style({
+  ...font.H6,
+  textAlign: "right",
+});
