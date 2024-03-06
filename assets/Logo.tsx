54428b3508d056b864a423d836c3d266c9f7a30a
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 15:41:13 +0900

    test,fix(header): i 태그 삭제, circleci 워크플로우 테스트

--- assets/Logo.tsx
+++ assets/Logo.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import React from "react";
+import React, { ImgHTMLAttributes } from "react";
 
-const Logo = () => {
-  return <Image src="/assets/logo.png" width={100} height={28} alt="logo" />;
+const Logo = ({ ...props }: ImgHTMLAttributes<HTMLImageElement>) => {
+  return (
+    <Image
+      {...props}
+      src="/assets/logo.png"
+      width={100}
+      height={28}
+      alt="logo"
+    />
+  );
 };
 
 export default Logo;
