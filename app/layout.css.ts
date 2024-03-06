6f1f11c35eb31f357a98bf1ae4cb895be3d2d028
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 15:29:51 +0900

    refactor(layout): 모바일 호환성 고려 svh 단위 사용

--- app/layout.css.ts
+++ app/layout.css.ts
@@ -3,7 +3,7 @@ import { style } from "@vanilla-extract/css";
 
 export const container = style({
   width: "100%",
-  minHeight: "100vh",
+  minHeight: "100svh",
   backgroundColor: theme.background,
   padding: "52px 20px 0 20px",
   ...flex.COLUMN_VERTICAL,
