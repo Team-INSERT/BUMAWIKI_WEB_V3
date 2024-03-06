ce8b6b841229974f4b76bf91011a49f828df26ef
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 17:12:09 +0900

    feat(footer): 푸터 컴포넌트 퍼블리싱

---
+++ components/Footer/style.css.ts
@@ -0,0 +1,32 @@
+import { font, flex } from "@/styles";
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  width: "100vw",
+  height: "22vh",
+  backgroundColor: "#fff",
+  border: "1px solid #ccc",
+  ...flex.COLUMN_CENTER,
+});
+
+export const footerLinkBox = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "10px 0",
+});
+
+export const separator = style({
+  margin: "0 20px 0 20px",
+  width: "2px",
+  height: "25px",
+  backgroundColor: "black",
+});
+
+export const informationBox = style({
+  marginTop: "12px",
+});
+
+export const information = style({
+  ...font.context,
+});
