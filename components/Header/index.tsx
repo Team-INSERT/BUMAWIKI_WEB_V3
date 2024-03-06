397947100bef726e97c377c05871e09a48b5c8b6
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 10:30:29 +0900

    feat(header): 헤더 컴포넌트 퍼블리싱

---
+++ components/Header/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Link from "next/link";
+import {
+  AccidentIcon,
+  ClubIcon,
+  FrameIcon,
+  PopularIcon,
+  StudentIcon,
+  TeacherIcon,
+  Logo,
+  SearchIcon,
+} from "@/assets";
+import * as styles from "./style.css";
+
+const navigationList = [
+  { item: "학생", href: "/student", icon: <StudentIcon /> },
+  { item: "선생님", href: "/teacher", icon: <TeacherIcon /> },
+  { item: "사건/사고", href: "/accident", icon: <AccidentIcon /> },
+  { item: "동아리", href: "/club", icon: <ClubIcon /> },
+  { item: "틀", href: "/frame", icon: <FrameIcon /> },
+  { item: "인기", href: "/popular", icon: <PopularIcon /> },
+];
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.navigationList}>
+        <Logo />
+        {navigationList.map((nav) => (
+          <Link href={nav.href} className={styles.navigationItem}>
+            {nav.icon}
+            <span>{nav.item}</span>
+          </Link>
+        ))}
+      </ul>
+      <form className={styles.searchBox}>
+        <input
+          placeholder="검색어를 입력하세요..."
+          className={styles.searchInput}
+        />
+        <i className={styles.searchButton}>
+          <SearchIcon />
+        </i>
+      </form>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Header/style.css.ts
@@ -0,0 +1,47 @@
+import { theme, flex } from "@/styles";
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  width: "100%",
+  padding: "0 4vw",
+  height: "54px",
+  backgroundColor: theme.primary,
+  position: "fixed",
+  ...flex.BETWEEN,
+});
+
+export const navigationList = style({
+  gap: "5vw",
+  ...flex.HORIZONTAL,
+});
+
+export const navigationItem = style({
+  color: theme.white,
+  fontWeight: 600,
+  gap: "6px",
+  ...flex.VERTICAL,
+  ":hover": {
+    opacity: 0.8,
+  },
+});
+
+export const searchBox = style({
+  position: "relative",
+  ...flex.END,
+});
+
+export const searchInput = style({
+  width: "16vw",
+  height: "30px",
+  position: "relative",
+  padding: "0 10px",
+  fontWeight: 500,
+  backgroundColor: theme.white,
+  border: `1px solid ${theme.gray}`,
+});
+
+export const searchButton = style({
+  position: "absolute",
+  right: "12px",
+  cursor: "pointer",
+});
