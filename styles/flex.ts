52288d94015a8698308fb8a007116748927a9baf
Ubinquitous <ubinquitous1@gmail.com>
2024-03-06 10:30:50 +0900

    feat(design-system): 디자인 시스템 세팅

---
+++ styles/flex.ts
@@ -0,0 +1,52 @@
+interface PropsType {
+  direction?: React.CSSProperties["flexDirection"];
+  justify?: React.CSSProperties["justifyContent"];
+  align?: React.CSSProperties["alignItems"];
+}
+
+const flexGenerator = ({ direction, align, justify }: PropsType) => ({
+  display: "flex",
+  flexDirection: direction,
+  justifyContent: justify,
+  alignItems: align,
+});
+
+const flex = {
+  FLEX: flexGenerator({ align: "none", justify: "none" }),
+  CENTER: flexGenerator({ align: "center", justify: "center" }),
+  VERTICAL: flexGenerator({ align: "center" }),
+  HORIZONTAL: flexGenerator({ justify: "center" }),
+  START: flexGenerator({ align: "center", justify: "flex-start" }),
+  BETWEEN: flexGenerator({ align: "center", justify: "space-between" }),
+  END: flexGenerator({ align: "center", justify: "flex-end" }),
+
+  COLUMN_FLEX: flexGenerator({
+    direction: "column",
+    align: "none",
+    justify: "none",
+  }),
+  COLUMN_CENTER: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "center",
+  }),
+  COLUMN_VERTICAL: flexGenerator({ direction: "column", align: "center" }),
+  COLUMN_HORIZONTAL: flexGenerator({ direction: "column", justify: "center" }),
+  COLUMN_START: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "flex-start",
+  }),
+  COLUMN_BETWEEN: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "space-between",
+  }),
+  COLUMN_END: flexGenerator({
+    direction: "column",
+    align: "center",
+    justify: "flex-end",
+  }),
+};
+
+export default flex;
---
+++ styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  primary: "#274168",
+  gray: "#ccc",
+  white: "#fff",
+  background: "#f5f5f5",
+  classify: "#EC9F19",
+  title: "#545454",
+  link: "#0038FF",
+};
+
+export default theme;
