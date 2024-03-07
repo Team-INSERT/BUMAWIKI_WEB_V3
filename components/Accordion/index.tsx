0202032a55314b02bc70fd2ab7c10f2649a599fd
Ubinquitous <ubinquitous1@gmail.com>
2024-03-07 20:25:20 +0900

    refactor(Accordion): 컴포넌트 인터페이스명 통일

--- components/Accordion/index.tsx
+++ components/Accordion/index.tsx
@@ -1,11 +1,11 @@
 import React, { PropsWithChildren } from "react";
 import * as styles from "./style.css";
 
-interface PropsType extends PropsWithChildren {
+interface AccordionProps extends PropsWithChildren {
   title: string;
 }
 
-const Accordion = ({ title, children }: PropsType) => {
+const Accordion = ({ title, children }: AccordionProps) => {
   return (
     <div className={styles.container}>
       <details className="details">
