efc45f377bd5ca687d161b276318ddb41850de06
jyh071116 <jyh071116@gmail.com>
2024-03-07 17:59:46 +0900

    feat(component): aside 컴포넌트 퍼블리싱

---
+++ components/Aside/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { ArrowIcon } from "@/assets";
+import * as styles from "./style.css";
+
+const Aside = () => {
+  return (
+    <aside className={styles.container}>
+      <div className={styles.titleBox}>
+        <span className={styles.titleText}>최근 수정된 문서</span>
+      </div>
+      {Array.from({ length: 12 }, (_, i) => (
+        <div key={i} className={styles.docs}>
+          <a className={styles.docsName} href="/">
+            한예준(학생)
+          </a>
+          <span className={styles.docsLastModified}>&nbsp; 3시간 전</span>
+        </div>
+      ))}
+      <div className={styles.pageBox}>
+        <button className={styles.pageButton}>
+          <span className={styles.pageButtonText}>
+            <ArrowIcon
+              direction="left"
+              width={11}
+              height={11}
+              viewBox="0 0 11 20"
+              fill="#274168"
+            />
+            이전
+          </span>
+        </button>
+        <button className={styles.pageButton}>
+          <span className={styles.pageButtonText}>
+            다음
+            <ArrowIcon
+              direction="right"
+              width={11}
+              height={11}
+              viewBox="0 0 11 20"
+              fill="#274168"
+            />
+          </span>
+        </button>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
---
+++ components/Aside/style.css.ts
@@ -0,0 +1,67 @@
+import { style } from "@vanilla-extract/css";
+import { flex, font, theme } from "@/styles";
+
+export const container = style({
+  position: "sticky",
+  width: "240px",
+  height: "fit-content",
+  marginTop: "20px",
+  top: "74px",
+});
+
+export const titleBox = style({
+  backgroundColor: theme.primary,
+  border: `2px solid ${theme.gray}`,
+  width: "100%",
+  height: "50px",
+  ...flex.VERTICAL,
+});
+
+export const titleText = style({
+  marginLeft: "20px",
+  color: theme.white,
+  ...font.H5,
+});
+
+export const docs = style({
+  width: "100%",
+  height: "40px",
+  backgroundColor: theme.white,
+  border: `2px solid ${theme.gray}`,
+  borderTop: "none",
+  ...flex.VERTICAL,
+});
+
+export const docsName = style({
+  color: theme.primary,
+  marginLeft: "10px",
+  fontSize: "14px",
+  fontWeight: 600,
+});
+
+export const docsLastModified = style({
+  color: theme.boldgray,
+  fontSize: "10px",
+});
+
+export const pageBox = style({
+  ...flex.FLEX,
+});
+
+export const pageButton = style({
+  width: "56px",
+  height: "24px",
+  backgroundColor: theme.white,
+  margin: "6px 2px",
+  border: `2px solid ${theme.gray}`,
+  borderRadius: "3px",
+  ...flex.CENTER,
+});
+
+export const pageButtonText = style({
+  color: theme.primary,
+  fontWeight: 600,
+  fontSize: "12px",
+  gap: "2.5px",
+  ...flex.CENTER,
+});
