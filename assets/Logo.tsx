631bc9c521a0b64ccf5bfaad438a478ae5d91d1e
Ubinquitous <ubinquitous1@gmail.com>
2024-03-07 20:12:33 +0900

    fix(header): 로고 클릭하면 홈화면으로 가게

--- assets/Logo.tsx
+++ assets/Logo.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { ImgHTMLAttributes } from "react";
 
 const Logo = ({ ...props }: ImgHTMLAttributes<HTMLImageElement>) => {
   return (
-    <Image
-      {...props}
-      src="/assets/logo.png"
-      width={100}
-      height={28}
-      alt="logo"
-    />
+    <Link href="/">
+      <Image
+        {...props}
+        src="/assets/logo.png"
+        width={100}
+        height={28}
+        alt="logo"
+      />
+    </Link>
   );
 };
 
