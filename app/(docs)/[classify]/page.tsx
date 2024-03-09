1cdd6d4ef69e483ed691dead579b4a16d2c9d7b0
Ubinquitous <ubinquitous1@gmail.com>
2024-03-09 21:07:20 +0900

    feat(docsList): docsList api 불러오는 로직 정의

---
+++ app/(docs)/[classify]/page.tsx
@@ -0,0 +1,30 @@
+import Container from "@/components/Container";
+import { useDocs } from "@/hooks/useDocs";
+import { docsQuery } from "@/services/docs/docsQuery";
+import React from "react";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import getQueryClient from "@/app/getQueryClient";
+import DocsList from "./DocsList";
+
+interface PageProps {
+  params: {
+    classify: string;
+  };
+}
+
+const Page = async ({ params: { classify } }: PageProps) => {
+  const { translateClassify } = useDocs();
+  const queryClient = getQueryClient();
+  const docsList = await queryClient.fetchQuery(docsQuery.getList(classify));
+
+  return (
+    <Container title={translateClassify(classify)} classify={classify}>
+      {classify}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <DocsList docsList={docsList} />
+      </HydrationBoundary>
+    </Container>
+  );
+};
+
+export default Page;
