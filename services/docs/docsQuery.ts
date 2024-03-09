16888f772761cd703ced76b056db4178740079e1
Ubinquitous <ubinquitous1@gmail.com>
2024-03-09 21:08:12 +0900

    chore(services): docs관련 services 추가

---
+++ services/docs/docsQuery.ts
@@ -0,0 +1,40 @@
+import DocsService from "./DocsService";
+
+const queryKeys = {
+  list: (classify: string) => ["list", classify] as const,
+  docs: (title: string) => ["docs", title] as const,
+};
+
+export const docsQuery = {
+  getList: (classify: string) => ({
+    queryKey: queryKeys.list(classify),
+    queryFn: () => DocsService.getList(classify).then((r) => r.data),
+  }),
+
+  getByTitle: (title: string) => ({
+    queryKey: queryKeys.docs(title),
+    queryFn: () => DocsService.getByTitle(title).then((r) => r.data),
+  }),
+
+  getByKeyword: (keyword: string) => ({
+    queryKey: queryKeys.docs(keyword),
+    queryFn: () => DocsService.getByKeyword(keyword).then((r) => r.data),
+  }),
+
+  create: () => ({
+    mutationFn: () => DocsService.create().then((r) => r.data),
+  }),
+
+  update: (title: string) => ({
+    mutationFn: () => DocsService.update(title).then((r) => r.data),
+  }),
+
+  updateTitle: (title: string, titleToChange: string) => ({
+    mutationFn: () =>
+      DocsService.updateTitle(title, titleToChange).then((r) => r.data),
+  }),
+
+  remove: (id: number) => ({
+    mutationFn: () => DocsService.remove(id).then((r) => r.data),
+  }),
+};
