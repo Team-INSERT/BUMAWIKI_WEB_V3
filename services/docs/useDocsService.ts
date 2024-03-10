f23027e2408074b94f59ca3391c0e5f7eea21990
Ubinquitous <ubinquitous1@gmail.com>
2024-03-11 08:30:07 +0900

    hotfix(services): useQuery import 누락

--- services/docs/useDocsService.ts
+++ services/docs/useDocsService.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { docsQuery } from "./docsQuery";
 
 export const useDocsList = ({ classify }: { classify: string }) => {
