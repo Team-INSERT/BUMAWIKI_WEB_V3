88042224404b5bebf99188e0bb404acee3fe2f87
Ubinquitous <ubinquitous1@gmail.com>
2024-03-14 11:19:04 +0900

    chore(services): user service 정의

---
+++ services/user/user.api.ts
@@ -0,0 +1,15 @@
+import { http } from "@/apis";
+import { TOKEN } from "@/constants/token.constant";
+import { Storage } from "@/storage";
+
+export const getMyInformation = async () => {
+  const { data } = await http.get("/user/", {
+    headers: { Authorization: Storage.getItem(TOKEN.ACCESS) },
+  });
+  return data;
+};
+
+export const getUserById = async (id: number) => {
+  const { data } = await http.get(`/user/id/${id}`);
+  return data;
+};
