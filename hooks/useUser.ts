d8047359f6a737364ee2594860c41315827b158c
Ubinquitous <ubinquitous1@gmail.com>
2024-03-14 11:18:02 +0900

    chore(user): user관련 state 정의

---
+++ hooks/useUser.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import { TOKEN } from "@/constants/token.constant";
+import { userContext } from "@/context";
+import { getMyInformation } from "@/services/user/user.api";
+import { Storage } from "@/storage";
+import { UserType } from "@/types/user.interface";
+import { useQuery } from "@tanstack/react-query";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+
+const useUser = () => {
+  const [user, setUser] = useAtom(userContext);
+
+  const { data: userInfo } = useQuery<UserType>({
+    queryKey: ["user"],
+    queryFn: getMyInformation,
+    enabled: !!Storage.getItem(TOKEN.ACCESS),
+  });
+
+  useEffect(() => {
+    if (userInfo) setUser(userInfo);
+  }, [setUser, userInfo]);
+
+  return {
+    user,
+    isLoggedIn: !!userInfo,
+    isAdmin: userInfo?.authority === "ADMIN",
+  };
+};
+
+export default useUser;
