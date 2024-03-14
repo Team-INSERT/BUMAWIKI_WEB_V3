18146bd0c9ca1af3af627b271276a7bd5cabd024
Ubinquitous <ubinquitous1@gmail.com>
2024-03-14 11:17:48 +0900

    style(prettier): prettier printWidth sizeup

--- components/Popular/index.tsx
+++ components/Popular/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { HydrationBoundary } from "@tanstack/react-query";
-import getQueryClient from "@/app/getQueryClient";
-import { docsQuery } from "@/services/docs/docsQuery";
 import { DocsListItem } from "@/types/docsListItem.interface";
 import Link from "next/link";
+import { useDocsListQuery } from "@/services/docs/docs.query";
 import * as styles from "./style.css";
 
 const Popular = async () => {
-  const queryClient = getQueryClient();
-  const popularList = await queryClient.fetchQuery(
-    docsQuery.getList("popular"),
-  );
+  const popularList = await useDocsListQuery({ classify: "popular" });
 
   return (
     <HydrationBoundary>
@@ -20,19 +16,14 @@ const Popular = async () => {
             <span className={styles.titleText}>실시간 인기</span>
           </div>
           <ul className={styles.docsList}>
-            {popularList
-              .slice(0, 10)
-              .map((popular: DocsListItem, index: number) => (
-                <li className={styles.docsListItem} key={popular.title}>
-                  <h1 className={styles.ranking}>{index + 1}</h1>
-                  <Link
-                    href={`/docs/${popular.title}`}
-                    className={styles.docsName}
-                  >
-                    {popular.title}
-                  </Link>
-                </li>
-              ))}
+            {popularList.slice(0, 10).map((popular: DocsListItem, index: number) => (
+              <li className={styles.docsListItem} key={popular.title}>
+                <h1 className={styles.ranking}>{index + 1}</h1>
+                <Link href={`/docs/${popular.title}`} className={styles.docsName}>
+                  {popular.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </aside>
       </div>
