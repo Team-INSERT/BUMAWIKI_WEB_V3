9952bf1f5f0077ca38f349929d935d0d4ddd97af
Ubinquitous <ubinquitous1@gmail.com>
2024-03-14 11:17:42 +0900

    chore(header): 검색 기능 연결

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import Link from "next/link";
 import {
   AccidentIcon,
@@ -13,6 +13,8 @@ import {
   MyPageIcon,
 } from "@/assets";
 import { useRouter } from "next/navigation";
+import { Storage } from "@/storage";
+import { TOKEN } from "@/constants/token.constant";
 import * as styles from "./style.css";
 
 const navigationList = [
@@ -25,47 +27,52 @@ const navigationList = [
 
 const Header = () => {
   const [keyword, setKeyword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
   const handleSubmitSearchByKeyword = (e: FormEvent) => {
     e.preventDefault();
     // 추후 커스텀 알레트로 수정
-    if (!keyword.trim()) alert("검색어를 입력해주세요!");
+    if (!keyword.trim()) return alert("검색어를 입력해주세요!");
     router.push(`/search/${keyword}`);
   };
+
+  useEffect(() => {
+    if (Storage.getItem(TOKEN.ACCESS)) setIsLoggedIn(true);
+  }, []);
 
   return (
     <div className={styles.container}>
       <ul className={styles.navigationList}>
         <Logo className={styles.logo} />
         {navigationList.map((nav) => (
-          <Link
-            key={nav.item}
-            href={nav.href}
-            className={styles.navigationItem}
-          >
+          <Link key={nav.item} href={nav.href} className={styles.navigationItem}>
             {nav.icon}
             <span>{nav.item}</span>
           </Link>
         ))}
       </ul>
       <div className={styles.utilityBox}>
-        <form
-          className={styles.searchBox}
-          onSubmit={handleSubmitSearchByKeyword}
-        >
+        <form className={styles.searchBox} onSubmit={handleSubmitSearchByKeyword}>
           <input
             onChange={({ target: { value } }) => setKeyword(value)}
             value={keyword}
             placeholder="검색어를 입력하세요..."
             className={styles.searchInput}
           />
-          <SearchIcon className={styles.searchButton} />
+          <SearchIcon onClick={handleSubmitSearchByKeyword} className={styles.searchButton} />
         </form>
-        <Link href="/mypage" className={styles.navigationItem}>
-          <MyPageIcon />
-          <span>내정보</span>
-        </Link>
+        {isLoggedIn ? (
+          <Link href="/mypage" className={styles.navigationItem}>
+            <MyPageIcon />
+            <span>내정보</span>
+          </Link>
+        ) : (
+          <Link href={process.env.NEXT_PUBLIC_OAUTH_URL || "/"} className={styles.navigationItem}>
+            <MyPageIcon />
+            <span>로그인</span>
+          </Link>
+        )}
       </div>
     </div>
   );
