4a3bbd61f1d98703828be467bd7b6356dc5802ce
5ewon06 <gwonsewon03@gmail.com>
2024-03-16 13:58:52 +0900

    feat(toastify): Toastify 컴포넌트 추가

---
+++ components/Tostify/index.tsx
@@ -0,0 +1,20 @@
+import InfoIcon from "@/assets/infoIcon";
+import * as styles from "./style.css";
+
+interface Props {
+  content: string;
+}
+
+const TostifyAlert = ({ content }: Props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentBox}>
+        <InfoIcon />
+        <div className={styles.content}>{content}</div>
+      </div>
+      <div className={styles.sideBar} />
+    </div>
+  );
+};
+
+export default TostifyAlert;
