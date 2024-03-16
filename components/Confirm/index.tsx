bf1c9a5cc31224ffe1e38953e31ecc56d74a1b04
5ewon06 <gwonsewon03@gmail.com>
2024-03-16 13:58:10 +0900

    feat(confirm): Confirm Alert 컴포넌트 추가

---
+++ components/Confirm/index.tsx
@@ -0,0 +1,42 @@
+import useModal from "@/modal/hook/useModal";
+import RoundLogo from "@/assets/RoundLogo";
+import * as styles from "./style.css";
+
+interface Props {
+  content: string;
+  onConfirm: () => void;
+}
+
+const Confirm = ({ content, onConfirm }: Props) => {
+  const { closeModal } = useModal();
+  const handleConfirm = () => {
+    onConfirm();
+    closeModal();
+  };
+
+  return (
+    <div className={styles.background} onClick={closeModal}>
+      <div className={styles.container} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.wrapper}>
+          <div className={styles.logoWrapper}>
+            <RoundLogo />
+          </div>
+          <div className={styles.contentBox}>
+            <div className={styles.content}>{content}</div>
+          </div>
+          <div className={styles.line} />
+          <div className={styles.buttonBox}>
+            <button className={styles.cancelBtn} onClick={closeModal}>
+              취소
+            </button>
+            <button className={styles.confirmBtn} onClick={handleConfirm}>
+              확인
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Confirm;
