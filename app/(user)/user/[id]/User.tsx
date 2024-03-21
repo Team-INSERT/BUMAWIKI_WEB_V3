834c0a75270f46eed1a6134b377f406933a5781f
Ubinquitous <ubinquitous1@gmail.com>
2024-03-21 10:56:32 +0900

    chore(user): 메타데이터 생성, api 구조 변경

--- app/(user)/user/[id]/User.tsx
+++ app/(user)/user/[id]/User.tsx
@@ -1,20 +1,28 @@
+"use client";
+
 import Accordion from "@/components/Accordion";
-import { UserType } from "@/types/user.interface";
-import React from "react";
+import React, { FC } from "react";
 import { 조사, particle } from "auto-particle";
 import { translateAuthority } from "@/utils";
+import Container from "@/components/Container";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { userQuery } from "@/services/user/user.query";
 import * as styles from "../../style.css";
 import ContritbuteDocsList from "../../ContritbuteDocsList";
 
-const User = ({ user }: { user: UserType }) => {
+const User: FC<{ id: number }> = ({ id }) => {
+  const { data: user } = useSuspenseQuery(userQuery.id(id));
+
   return (
-    <div className={styles.container}>
-      <Accordion title="유저 정보">
-        {particle(user.nickName).word(조사.은_는)} 부마위키의 {translateAuthority(user.authority)}
-        이다.
-      </Accordion>
-      <ContritbuteDocsList contributes={user.contributeDocs} />
-    </div>
+    <Container title={user.nickName} docsType="user">
+      <div className={styles.container}>
+        <Accordion title="유저 정보">
+          {particle(user.nickName).word(조사.은_는)} 부마위키의 {translateAuthority(user.authority)}
+          이다.
+        </Accordion>
+        <ContritbuteDocsList contributes={user.contributeDocs} />
+      </div>
+    </Container>
   );
 };
 
