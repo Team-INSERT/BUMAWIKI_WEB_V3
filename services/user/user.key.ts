8f0e856ae4b05e524aeaf45e8dd38f68f93efafd
Ubinquitous <ubinquitous1@gmail.com>
2024-03-21 11:01:35 +0900

    chore(user): user service queryOptions로 구조 변경

--- services/user/user.key.ts
+++
@@ -1,4 +0,0 @@
-export const USER = {
-  ID: (id: number) => ["userInfo", id],
-  MY: ["userMyInfo"],
-};
--- services/user/user.query.ts
+++ services/user/user.query.ts
@@ -1,19 +1,15 @@
-import getQueryClient from "@/app/getQueryClient";
-import { useQuery } from "@tanstack/react-query";
+import { queryOptions } from "@tanstack/react-query";
 import { getMyInformation, getUserById } from "./user.api";
-import { USER } from "./user.key";
 
-export const useMyInformationQuery = () => {
-  return useQuery({
-    queryKey: USER.MY,
-    queryFn: getMyInformation,
-  });
-};
-
-export const useUserByIdQuery = ({ id }: { id: number }) => {
-  const queryClient = getQueryClient();
-  return queryClient.fetchQuery({
-    queryKey: USER.ID(id),
-    queryFn: () => getUserById(id),
-  });
+export const userQuery = {
+  my: () =>
+    queryOptions({
+      queryKey: ["query.my"],
+      queryFn: getMyInformation,
+    }),
+  id: <Id extends number>(id: Id) =>
+    queryOptions({
+      queryKey: ["query.user", id],
+      queryFn: () => getUserById(id),
+    }),
 };
