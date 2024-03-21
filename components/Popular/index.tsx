5320300a49ff7bfb664d1a3253878516e69840f6
Ubinquitous <ubinquitous1@gmail.com>
2024-03-21 10:58:52 +0900

    chore(popular): SSR로 렌더링방식 변경

--- components/Popular/index.tsx
+++ components/Popular/index.tsx
@@ -1,34 +1,18 @@
 import React from "react";
-import { HydrationBoundary } from "@tanstack/react-query";
-import { DocsListItem } from "@/types/docsListItem.interface";
-import Link from "next/link";
-import { useDocsListQuery } from "@/services/docs/docs.query";
-import * as styles from "./style.css";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import getQueryClient from "@/app/getQueryClient";
+import { docsQuery } from "@/services/docs/docs.query";
+import Popular from "./Popular";
 
-const Popular = async () => {
-  const popularList = await useDocsListQuery({ classify: "popular" });
+const Page = async () => {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(docsQuery.list("popular"));
 
   return (
-    <HydrationBoundary>
-      <div className={styles.body}>
-        <aside className={styles.container}>
-          <div className={styles.titleBox}>
-            <span className={styles.titleText}>실시간 인기</span>
-          </div>
-          <ul className={styles.docsList}>
-            {popularList.slice(0, 10).map((popular: DocsListItem, index: number) => (
-              <li className={styles.docsListItem} key={popular.title}>
-                <h1 className={styles.ranking}>{index + 1}</h1>
-                <Link href={`/docs/${popular.title}`} className={styles.docsName}>
-                  {popular.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </aside>
-      </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Popular />
     </HydrationBoundary>
   );
 };
 
-export default Popular;
+export default Page;
