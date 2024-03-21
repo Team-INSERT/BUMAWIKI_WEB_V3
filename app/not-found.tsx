351d5f34910e1a8e8abf007228cc8da81f162d45
Ubinquitous <ubinquitous1@gmail.com>
2024-03-21 10:54:37 +0900

    feat(404): not found ui 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <Container title="404" docsType="NOT FOUND">
+      해당 페이지를 찾을 수 없습니다.
+    </Container>
+  );
+};
+
+export default NotFound;
