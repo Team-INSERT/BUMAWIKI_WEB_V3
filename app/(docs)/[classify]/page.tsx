e97941f3ca82814d9bdbfaf5e18325ad885b27f6
Ubinquitous <ubinquitous1@gmail.com>
2024-03-21 10:56:10 +0900

    chore([classify]): 메타데이터 생성, api 구조 변경

--- app/(docs)/[classify]/page.tsx
+++ app/(docs)/[classify]/page.tsx
@@ -1,8 +1,9 @@
-import Container from "@/components/Container";
-import { useDocs } from "@/hooks/useDocs";
 import React from "react";
-import { HydrationBoundary } from "@tanstack/react-query";
-import { useDocsListQuery } from "@/services/docs/docs.query";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import getQueryClient from "@/app/getQueryClient";
+import { docsQuery } from "@/services/docs/docs.query";
+import { generateOpenGraph, translateClassify } from "@/utils";
+import { Metadata } from "next";
 import DocsList from "./DocsList";
 
 interface PageProps {
@@ -11,16 +12,21 @@ interface PageProps {
   };
 }
 
+export const generateMetadata = async ({ params: { classify } }: PageProps): Promise<Metadata> => {
+  return generateOpenGraph({
+    title: translateClassify(classify),
+    description: `교내의 ${translateClassify(classify)}들을 모아둔 페이지입니다.`,
+  });
+};
+
 const Page = async ({ params: { classify } }: PageProps) => {
-  const { translateClassify } = useDocs();
-  const docsList = await useDocsListQuery({ classify });
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(docsQuery.list(classify));
 
   return (
-    <Container title={translateClassify(classify)} docsType={classify}>
-      <HydrationBoundary>
-        <DocsList docsList={docsList} />
-      </HydrationBoundary>
-    </Container>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DocsList classify={classify} />
+    </HydrationBoundary>
   );
 };
 
