fe238498d18494d1c56fdfc8f3f1b2bda844b49c
Ubinquitous <ubinquitous1@gmail.com>
2024-03-28 09:57:26 +0900

    chore(services): coin services 정의

---
+++ services/coin/coin.mutation.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  cancelTrade,
+  requestBuyCoin,
+  requestCreateCoinWallet,
+  requestDailyReward,
+  requestSellCoin,
+} from "./coin.api";
+
+export const useCreateCoinWalletMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: requestCreateCoinWallet,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["query.myWallet"] });
+    },
+  });
+};
+
+export const useBuyCoinMutation = () => {
+  return useMutation({
+    mutationFn: requestBuyCoin,
+  });
+};
+
+export const useSellMutation = () => {
+  return useMutation({
+    mutationFn: requestSellCoin,
+  });
+};
+
+export const useDailyRewardMutation = () => {
+  return useMutation({
+    mutationFn: requestDailyReward,
+  });
+};
+
+export const useCancelTradeMutation = () => {
+  return useMutation({
+    mutationFn: cancelTrade,
+  });
+};
