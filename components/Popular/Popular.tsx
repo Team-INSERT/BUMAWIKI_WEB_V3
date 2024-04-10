3a34e8f68caf5b4f9f5ce094260963e690b6435d
Ubinquitous <ubinquitous1@gmail.com>
2024-04-10 20:38:15 +0900

    refactor(popular): 시멘틱 태그 적용, 불필요한 코드 대폭 제거, 구성요소 변경

--- components/Popular/Popular.tsx
+++ components/Popular/Popular.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { useState } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { docsQuery } from "@/services/docs/docs.query";
 import Link from "next/link";
@@ -10,60 +10,36 @@ import * as styles from "./style.css";
 const Popular = () => {
   const { data: popularList } = useSuspenseQuery(docsQuery.list("popular"));
   const [isListOpen, setIsListOpen] = useState(false);
+  const containerStatus = isListOpen ? "open" : "close";
+
+  const handlePopularListMouseHover = () => {
+    setIsListOpen((prev) => !prev);
+  };
 
   return (
-    <Suspense>
-      <div
-        onMouseEnter={() => setIsListOpen((prev) => !prev)}
-        onMouseLeave={() => setIsListOpen((prev) => !prev)}
-        className={styles.body}
-      >
-        {isListOpen && (
-          <aside className={styles.openContainer}>
-            <div className={styles.titleOpenBox}>
-              <span className={styles.titleText}>인기</span>
+    <aside
+      onMouseEnter={handlePopularListMouseHover}
+      onMouseLeave={handlePopularListMouseHover}
+      className={styles.container[containerStatus]}
+    >
+      <header className={styles.titleBox}>인기</header>
+      <ul className={styles.docsList}>
+        {popularList.slice(0, 10).map((popular, index) => (
+          <Link
+            href={`/docs/${popular.title}`}
+            className={styles.docsListItem[containerStatus]}
+            key={popular.title}
+          >
+            <h1 className={styles.ranking}>{index + 1}</h1>
+            <span className={styles.docsName}>{popular.title}</span>
+            <div className={styles.thumbsUpsCountsBox}>
+              <LikeIcon isLike width={14} height={14} />
+              <span>{popular.thumbsUpsCounts}</span>
             </div>
-            <ul className={styles.docsOpenList}>
-              {popularList.map((popular, index) => (
-                <Link
-                  href={`/docs/${popular.title}`}
-                  className={styles.docsOpenListItem}
-                  key={popular.title}
-                >
-                  <h1 className={styles.ranking}>{index + 1}</h1>
-                  <span className={styles.docsName}>{popular.title}</span>
-                  <div className={styles.thumbsUpsCountsBox}>
-                    <LikeIcon isLike width={14} height={14} />
-                    <span>{popular.thumbsUpsCounts}</span>
-                  </div>
-                </Link>
-              ))}
-            </ul>
-          </aside>
-        )}
-        <aside className={styles.container}>
-          <div className={styles.titleBox}>
-            <span className={styles.titleText}>인기</span>
-          </div>
-          <ul className={styles.docsList}>
-            {popularList.slice(0, 10).map((popular, index) => (
-              <Link
-                href={`/docs/${popular.title}`}
-                className={styles.docsListItem}
-                key={popular.title}
-              >
-                <h1 className={styles.ranking}>{index + 1}</h1>
-                <span className={styles.docsName}>{popular.title}</span>
-                <div className={styles.thumbsUpsCountsBox}>
-                  <LikeIcon isLike width={14} height={14} />
-                  <span>{popular.thumbsUpsCounts}</span>
-                </div>
-              </Link>
-            ))}
-          </ul>
-        </aside>
-      </div>
-    </Suspense>
+          </Link>
+        ))}
+      </ul>
+    </aside>
   );
 };
 
--- components/Popular/style.css.ts
+++ components/Popular/style.css.ts
@@ -1,88 +1,52 @@
-import { style } from "@vanilla-extract/css";
+import { style, styleVariants } from "@vanilla-extract/css";
 import { flex, font, theme } from "@/styles";
+import StyleVariantsType from "@/types/styleVariants.interface";
 import { popularAnimation } from "./keyframes.css";
 
-export const body = style({
+const containerBase = style({
+  background: theme.white,
   width: "300px",
   height: "90px",
-  ...flex.COLUMN_FLEX,
-});
-
-export const container = style({
-  background: theme.white,
-  border: `2px solid ${theme.gray}`,
-  height: "100%",
   borderTop: "none",
-  overflow: "hidden",
-  position: "relative",
+  zIndex: 100,
 });
 
-export const openContainer = style({
-  background: theme.white,
-  height: "100%",
-  position: "relative",
+export const container = styleVariants<StyleVariantsType>({
+  open: [containerBase],
+  close: [containerBase, { overflow: "hidden", borderBottom: `2px solid ${theme.gray}` }],
 });
 
 export const titleBox = style({
   backgroundColor: theme.primary,
-  border: `2px solid ${theme.gray}`,
   borderLeft: "none",
   borderRight: "none",
   width: "100%",
   height: "46px",
-  ...flex.VERTICAL,
-});
-
-export const titleOpenBox = style({
-  backgroundColor: theme.primary,
-  border: `2px solid ${theme.gray}`,
-  borderBottom: "none",
-  width: "100%",
-  height: "46px",
-  ...flex.VERTICAL,
-});
-
-export const titleText = style({
-  marginLeft: "20px",
+  padding: "0 20px",
   color: theme.white,
   ...font.H5,
+  ...flex.VERTICAL,
 });
 
 export const docsList = style({
   overflow: "hidden",
-  ...flex.COLUMN_FLEX,
-});
-
-export const docsOpenList = style({
-  backgroundColor: theme.white,
-  zIndex: 500,
   border: `2px solid ${theme.gray}`,
   ...flex.COLUMN_FLEX,
 });
 
-export const docsListItem = style({
+const docsListItemBase = style({
   width: "100%",
   height: "44px",
   padding: "0 16px",
   gap: "12px",
   backgroundColor: theme.white,
-  animationDuration: "20s",
-  animationFillMode: "forwards",
-  animationIterationCount: "infinite",
-  animationName: popularAnimation,
-  animationTimingFunction: "ease-in-out",
   cursor: "pointer",
   ...flex.VERTICAL,
 });
 
-export const docsOpenListItem = style({
-  width: "100%",
-  height: "44px",
-  padding: "0 16px",
-  gap: "12px",
-  backgroundColor: theme.white,
-  cursor: "pointer",
-  ...flex.VERTICAL,
+export const docsListItem = styleVariants<StyleVariantsType>({
+  open: [docsListItemBase],
+  close: [docsListItemBase, { animation: `${popularAnimation} 20s infinite forwards ease-in-out` }],
 });
 
 export const ranking = style({
@@ -108,7 +72,6 @@ export const thumbsUpsCountsBox = style({
 export const hoverList = style({
   width: "400px",
   height: "400px",
-  backgroundColor: "green",
   position: "absolute",
   zIndex: 50,
 });
