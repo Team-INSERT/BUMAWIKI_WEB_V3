4a76a5a36a31b04e9b707703a2f8d77c08e5d045
5ewon06 <gwonsewon03@gmail.com>
2024-04-13 16:22:08 +0900

    feat(toastify): 인포 아이콘 반응형 css 사이즈 지정

--- components/Toastify/index.tsx
+++ components/Toastify/index.tsx
@@ -4,7 +4,7 @@ import * as styles from "./style.css";
 const Toastify = ({ content }: { content: string }) => {
   return (
     <figure className={styles.container}>
-      <Image width={24} height={24} src="/assets/info.png" alt="INFO" />
+      <Image width={24} height={24} src="/assets/info.png" alt="INFO" className={styles.icon} />
       <figcaption className={styles.content}>{content}</figcaption>
     </figure>
   );
