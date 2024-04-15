811a40cffe5a5694d1ad7d55350f93dbd8129c37
Ubinquitous <ubinquitous1@gmail.com>
2024-04-15 09:56:24 +0900

    refactor(User): 필요없는 태그 삭제

--- app/(user)/user/[id]/User.tsx
+++ app/(user)/user/[id]/User.tsx
@@ -6,22 +6,19 @@ import { 조사, particle } from "auto-particle";
 import Container from "@/components/Container";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { userQuery } from "@/services/user/user.query";
-import { ROLE } from "@/record";
-import * as styles from "../../style.css";
+import { CLASSIFY, ROLE } from "@/record";
 import ContritbuteDocsList from "../../ContritbuteDocsList";
 
 const User: FC<{ id: number }> = ({ id }) => {
   const { data: user } = useSuspenseQuery(userQuery.id(id));
 
   return (
-    <Container title={user.nickName} docsType="user">
-      <div className={styles.container}>
-        <Accordion title="유저 정보">
-          {particle(user.nickName).word(조사.은_는)} 부마위키의 {ROLE[user.authority]}
-          이다.
-        </Accordion>
-        <ContritbuteDocsList contributes={user.contributeDocs} />
-      </div>
+    <Container title={user.nickName} docsType={CLASSIFY.유저}>
+      <Accordion title="유저 정보">
+        {particle(user.nickName).word(조사.은_는)} 부마위키의 {ROLE[user.authority]}
+        이다.
+      </Accordion>
+      <ContritbuteDocsList contributes={user.contributeDocs} />
     </Container>
   );
 };
