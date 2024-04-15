0768c0f70f352e8f6e1f321a95c186f2531dcdce
Ubinquitous <ubinquitous1@gmail.com>
2024-04-15 09:55:59 +0900

    refactor(MyPage): 조건문 캡슐화

--- app/(user)/mypage/MyPage.tsx
+++ app/(user)/mypage/MyPage.tsx
@@ -4,21 +4,21 @@ import Accordion from "@/components/Accordion";
 import Container from "@/components/Container";
 import { useLogoutMutation } from "@/services/auth/auth.mutation";
 import { userQuery } from "@/services/user/user.query";
-import { useQueries } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { particle, 조사 } from "auto-particle";
-import { ROLE } from "@/record";
+import { CLASSIFY, ROLE } from "@/record";
 import Link from "next/link";
 import ContritbuteDocsList from "../ContritbuteDocsList";
 import * as styles from "../style.css";
 import LikeDocsList from "../LikeDocsList";
 
 const MyPage = () => {
-  const [{ data: user }, { data: likeList }] = useQueries({
-    queries: [userQuery.my(), userQuery.like()],
-  });
+  const { data: user } = useQuery(userQuery.my());
+  const { data: likeList } = useQuery(userQuery.like());
+  const isLoggedIn = user && likeList;
   const { mutate } = useLogoutMutation();
 
-  if (!user || !likeList)
+  if (!isLoggedIn)
     return (
       <Container title="마이페이지" docsType="mypage">
         <Link className={styles.link} href={process.env.NEXT_PUBLIC_OAUTH_URL || ""}>
@@ -28,18 +28,16 @@ const MyPage = () => {
     );
 
   return (
-    <Container title="마이페이지" docsType="mypage">
-      <div className={styles.container}>
-        <Accordion title="내정보">
-          {particle(user.nickName).word(조사.은_는)} 부마위키의 {ROLE[user.authority]}
-          이다.
-          <button onClick={() => mutate()} className={styles.button}>
-            로그아웃
-          </button>
-        </Accordion>
-        <LikeDocsList likeList={likeList} />
-        <ContritbuteDocsList contributes={user.contributeDocs} />
-      </div>
+    <Container title="마이페이지" docsType={CLASSIFY.마이페이지}>
+      <Accordion title="내정보">
+        {particle(user.nickName).word(조사.은_는)} 부마위키의 {ROLE[user.authority]}
+        이다.
+        <button onClick={() => mutate()} className={styles.button}>
+          로그아웃
+        </button>
+      </Accordion>
+      <LikeDocsList likeList={likeList} />
+      <ContritbuteDocsList contributes={user.contributeDocs} />
     </Container>
   );
 };
