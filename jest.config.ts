dcabe8927b94f6b36faf16456dac8c2a508f9aa1
ubinquitous <ubinquitous1@gmail.com>
2024-08-16 15:08:59 +0900

    feat : jest setting

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+};
+
+export default createJestConfig(config);
