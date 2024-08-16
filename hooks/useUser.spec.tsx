ee45f8f704fac58c47aae211e915395fbfe7a35e
ubinquitous <ubinquitous1@gmail.com>
2024-08-16 16:00:24 +0900

    chore : 사용하지 않는 주석 삭제

--- hooks/useUser.spec.tsx
+++ hooks/useUser.spec.tsx
@@ -1,4 +1,3 @@
-// useUser.test.js
 import { act, renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
